2b67bcf64e38e5cc92f5432dd465eee798fb2fcf
namitgpta <namitg677@gmail.com>
2022-06-29 16:31:25 +0530

    format changes

--- AttendanceBooster.py
+++ AttendanceBooster.py
@@ -4,32 +4,35 @@ import sys
 import csv
 import os
 
+
 def find_next_class(timetable_location, day):
     current_time = datetime.now().strftime("%H:%M")
     curr_time = time.strptime(current_time, "%H:%M")
-    
+
     with open(timetable_location, 'r') as timetable:
         reader = csv.reader(timetable)
         for row in reader:
             class_time = time.strptime(str(row[0]), "%H:%M")
-            
+
             if class_time >= curr_time and row[day+1] == "exit":
                 print("No more classes for today.")
                 exit()
             elif class_time >= curr_time and row[day+1] != "":
                 print("Next Class at " + row[0])
                 return class_time, row[day+1]
-    
+
     print("Classes already over.")
     exit()
 
+
 def attend_class(timetable_location):
-    Weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    Weekdays = ("Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday")
     day = datetime.today().weekday()
     print(Weekdays[day])
 
     while True:
-        class_time, link = find_next_class(timetable_location, day) 
+        class_time, link = find_next_class(timetable_location, day)
         while True:
             current_time = datetime.now().strftime("%H:%M")
             curr_time = time.strptime(current_time, "%H:%M")
@@ -43,5 +46,6 @@ def attend_class(timetable_location):
                 break
 
             time.sleep(60)
+
 
 attend_class(sys.argv[1])
